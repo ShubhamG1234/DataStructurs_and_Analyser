4091f83f43cc32c53d7b44ffb00a832a45ce753f
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-05-18 23:02:57 +0530

    prime numbers

--- Logical/Logical/Program.cs
+++ Logical/Logical/Program.cs
@@ -1,2 +1,19 @@
 ﻿
 Console.WriteLine("Welcome to the Prime Numbers !");
+{
+    int n, i, m = 0, sum = 0;
+    Console.Write("Enter the Number to check Prime: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    m = n / 2;
+    for (i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            Console.Write("Number is not Prime.");
+            sum = 1;
+            break;
+        }
+    }
+    if (sum == 0)
+        Console.Write("Number is Prime.");
+}
